76ac14e08e7f5780491aa89e3118b33d98b2cd6a
VincentPaulV <vincentpaulpersonal@gmail.com>
2023-11-10 18:43:47 +0530

    Station: Raspberry PI Changes: app_trial.js is the speed control for servo motor(WE can change the speed of the motor!) Features: Data Acquistion from sensor every 10 ms and Actuation happens for 10 seconds on both stepper and servo motor and data is plotted on plotPage.html

--- app_new.js
+++ app_new.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const { Server } = require("socket.io");
+var servo_motor = require('./app_trial')
 // For the sensor:
 var i2c = require('i2c-bus');
 var MPU6050 = require('i2c-mpu6050');
@@ -11,7 +12,7 @@ var i2c1 = i2c.openSync(1);
 var mpu = new MPU6050(i2c1, address);
 const bodyParser = require("body-parser");
 
-//
+
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,47 +22,43 @@ app.get("/", (req, res) => {
   });
 
   //For the mototr driver:
-var rpio = require('rpio');
+const rpio = require('rpio');
 var in1Pin = 11;
 var in2Pin = 15;
 var in3Pin = 16;
 var in4Pin = 18;
-
 // //Setup done:
-rpio.open(in1Pin,rpio.OUTPUT)
+// rpio.open(in1Pin,rpio.OUTPUT)
 rpio.open(29,rpio.OUTPUT)
-rpio.open(in2Pin,rpio.OUTPUT)
-rpio.open(in3Pin,rpio.OUTPUT)
-rpio.open(in4Pin,rpio.OUTPUT)
+// rpio.open(in2Pin,rpio.OUTPUT)
+// rpio.open(in3Pin,rpio.OUTPUT)
+// rpio.open(in4Pin,rpio.OUTPUT)
 
-rpio.mode(in1Pin, rpio.LOW);
-rpio.mode(in2Pin, rpio.LOW);
-rpio.mode(in3Pin, rpio.LOW);
-rpio.mode(in4Pin, rpio.LOW);
+// function setMotorSpeed(speed1, speed2) {
+//   const delay1 = Math.floor(1000000 / speed1);
+//   const delay2 = Math.floor(1000000 / speed2);
+//   rpio.write(in1Pin, rpio.HIGH);
+//   rpio.write(in3Pin, rpio.HIGH);
+//   rpio.usleep(delay1);
+//   rpio.usleep(delay2);
+//   rpio.write(in1Pin, rpio.LOW);
+//   rpio.write(in3Pin, rpio.LOW);
+// }
 
+// function setMotorDirection(direction1, direction2) {
+//   rpio.write(in2Pin, direction1);
+//   rpio.write(in4Pin, direction2);
+// }
 // // rpio.mode(29, rpio.LOW);
 // rpio.mode(29, rpio.HIGH);
 
-const io = new Server(3003)
+// const io = new Server(3003)
 
-io.on("connection",(socket) => { setInterval(()=> {socket.emit("hello",mpu.readSync()['accel']['z'])} ,10);
-});
+// io.on("connection",(socket) => { setInterval(()=> {socket.emit("hello",mpu.readSync()['accel']['z'])} ,10);
+// });
+
 app.get('/run-motor', (req, res) => {
-  // Turn on the motor.
-  rpio.mode(in1Pin, rpio.HIGH);
-  rpio.mode(in2Pin, rpio.HIGH);
-  rpio.mode(in3Pin, rpio.HIGH);
-  rpio.mode(in4Pin, rpio.HIGH);
-
-  // Wait for 1 second.
-  setTimeout(() => {
-    // Turn off the motor.
-    rpio.mode(in1Pin, rpio.LOW);
-    rpio.mode(in2Pin, rpio.LOW);
-    rpio.mode(in3Pin, rpio.LOW);
-    rpio.mode(in4Pin, rpio.LOW);
-  }, 5000);
-
+  servo_motor.run(50,100);
   res.send('Motor is running!');
 });
 
@@ -72,20 +69,20 @@ app.get('/run-stepmotor', (req, res) => {
     // Wait for 10 milliseconds
   setTimeout(() => {
     // Set the GPIO pin 5 to low
-    rpio.write(29, rpio.LOW);
+    rpio.mode(29, rpio.LOW);
   }, 10);
 
   // Loop forever
   setInterval(() => {
     // Set the GPIO pin 5 to high
-    rpio.write(29, rpio.HIGH);
+    rpio.mode(29, rpio.HIGH);
 
     // Wait for 10 milliseconds
     setTimeout(() => {
       // Set the GPIO pin 5 to low
-      rpio.write(29, rpio.LOW);
-    }, 1);
-  }, 1);
+      rpio.mode(29, rpio.LOW);
+    }, 10);
+  }, 10);
 
   res.send('Stepper Motor is running!');
 });
@@ -181,6 +178,5 @@ app.get('/data',function(req,res){
 
 })
 
-//Hemlo!!
   
 app.listen(3000, "0.0.0.0");
